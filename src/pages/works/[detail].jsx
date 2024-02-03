fe5efbc11e660d5d2bf47130223472eeb0aff30d
Riku <rikudonoyamaguchi@gmail.com>
2024-02-03 20:39:06 +0900

    fix detail

--- src/pages/works/[detail].jsx
+++ src/pages/works/[detail].jsx
@@ -8,70 +8,12 @@ import Link from "next/link";
 
 const ENDPOINT = "http://127.0.0.1:10013/wp-json/wp/v2/posts";
 
-const Detail = ({ article }) => {
-  const myLoader = ({ src, width, quality }) => {
-    return `${src}?w=${width}&q=${quality || 75}`;
-  };
+const Detail = () => {
   return (
     <>
-      <Head>
-        <title>
-          {article[0].title.rendered} | 三重県フリーランスエンジニア　大山口吏紅
-          | Webサイト制作
-        </title>
-      </Head>
-      <Header />
-      <h1 className="page-name">DETAILS</h1>
-      <section className="single-content">
-        <div className="single-image">
-          <Image
-            loader={myLoader}
-            src={article[0].acf.thumbnail_of_work}
-            width={1154}
-            height={718}
-            alt=""
-          />
-        </div>
-        <h2>{article[0].acf.cliant_name}</h2>
-        <div className="single-category wordpress">WordPress</div>
-        <div
-          dangerouslySetInnerHTML={{ __html: article[0].content.rendered }}
-        />
-        <div className="single-button">
-          <Link href="/works">一覧に戻る ＞</Link>
-        </div>
-      </section>
-      <Contactsection />
-      <Footer />
+      <p>このサイトにはアクセスできません</p>
     </>
   );
-};
-
-export const getStaticPaths = async () => {
-  const result = await axios.get(`${ENDPOINT}?per_page=100`).then((res) => {
-    return res.data;
-  });
-
-  const Paths = result.map((article) => ({
-    params: { detail: `${article.slug}` },
-  }));
-  const paths = [...Paths];
-
-  return {
-    paths,
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async ({ params }) => {
-  const url = `${ENDPOINT}?slug=${params.detail}`;
-  const result = await axios.get(url).then((res) => {
-    return res.data;
-  });
-
-  return {
-    props: { article: result },
-  };
 };
 
 export default Detail;
